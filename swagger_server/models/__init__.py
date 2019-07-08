9f98ac07b210f9ea15b2f5bee369c0a0bb417288
hcvazquez <hernan.c.vazquez@gmail.com>
2019-07-08 16:47:51 -0300

    Initial commit

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.handling_time import HandlingTime
+from swagger_server.models.purchase import Purchase
