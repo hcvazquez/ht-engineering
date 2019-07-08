a017f085cf9e2ef35cac8da25768af4d02d289f4
hcvazquez <hernan.c.vazquez@gmail.com>
2019-07-08 17:04:28 -0300

    Initial commit

--- swagger_server/controllers/linear_regression_controller.py
+++ swagger_server/controllers/linear_regression_controller.py
@@ -1,15 +1,12 @@
 import connexion
-import six
 
-from swagger_server.models.handling_time import HandlingTime  # noqa: E501
-from swagger_server.models.purchase import Purchase  # noqa: E501
-from swagger_server import util
+from swagger_server.models.handling_time import HandlingTime
+from swagger_server.models.purchase import Purchase
 
-from pyspark.sql import SparkSession
 from pyspark.ml.regression import LinearRegressionModel
 
 
-def get_linear_reg_prediction(body):  # noqa: E501
+def get_linear_reg_prediction(body):
     """Get handling time using Linear Regression
 
     Purchase for predicting handling-time # noqa: E501
@@ -19,11 +16,11 @@ def get_linear_reg_prediction(body):  # noqa: E501
 
     :rtype: HandlingTime
     """
-    
-    # Load Linear Regression model 
+
+    # Load Linear Regression model
     model = LinearRegressionModel.load('prod_model')
-    
+
     if connexion.request.is_json:
-        body = Purchase.from_dict(connexion.request.get_json())  # noqa: E501
-        
+        body = Purchase.from_dict(connexion.request.get_json())
+
     return HandlingTime(model.evaluate(**body))
