44e81b258e7b117e9622169a1566863a9adb7c91
hcvazquez <hernan.c.vazquez@gmail.com>
2019-07-08 16:45:01 -0300

    Initial commit

---
+++ data_engineering/airflow-pipeline.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+import os
+
+srcDir = os.getcwd() + '/'
+sparkSubmit = '/usr/local/spark/bin/spark-submit'
+
+## Define the DAG object
+default_args = {
+    'owner': 'hcvazquez',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 7, 15),
+}
+dag = DAG('handlingTimeDataEngineering', default_args=default_args, schedule_interval=timedelta(1))
+
+'''
+Defining three tasks: one task to collect the data
+one task to do feature engineering and
+one task to create and store the model.
+'''
+
+collectData= BashOperator(
+    task_id='spark-collect-data',
+    bash_command='python ' + srcDir + 'data_engineering/spark-collect-data.py ',
+    dag=dag)
+
+
+featureEngineering = BashOperator(
+    task_id='spark-feature-engineering',
+    bash_command=sparkSubmit + ' ' + srcDir + 'data_engineering/spark-feature-engineering.py ',
+    dag=dag)
+
+featureEngineering.set_upstream(collectData)
+
+
+buildingModel = BashOperator(
+    task_id='spark-building-model',
+    bash_command=sparkSubmit + ' ' + srcDir + 'data_engineering/spark-building-model.py ',
+    dag=dag)
+
+buildingModel.set_upstream(featureEngineering)
